6a0c3347ec2765fca3482f0d5276578279475dd6
ApiPeralta <69977662+ApiPeralta@users.noreply.github.com>
2021-05-04 17:21:46 -0300

    Primer movimiento (tardio)
    Clone repositorio, puse gitignore, e hice un enemigo.

---
+++ Assets/Scripts/Enemys/Controller_Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Controller_Enemy : MonoBehaviour
+{
+    public static float enemyVelocity = 3;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rb.AddForce(new Vector3(-enemyVelocity, 0, 0), ForceMode.Force);
+        //OutOfBounds();
+    }
+    //public void OutOfBounds()
+    //{
+    //    if (this.transform.position.x <= -40)
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+}
---
+++ Assets/Scripts/Enemys/Enemy_Instantiator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Instantiator : MonoBehaviour
+{
+    public List<GameObject> enemies;
+    public GameObject instantiatePos;
+    public float respawningTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnEnemies();
+    }
+    private void SpawnEnemies()                                
+    {
+        respawningTimer -= Time.deltaTime;
+        if (respawningTimer <= 0)
+        {
+            Instantiate(enemies[GameManager.actualPlayer], instantiatePos.transform);           
+            respawningTimer = UnityEngine.Random.Range(3, 6);
+        }
+    }
+}
